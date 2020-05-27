65cd6bbef4b9ce6c498e8a02cb1972024a315bdf
Hampus Hallkivst <hampus@hallkvist.org>
2020-05-27 22:51:26 +0200

    Implemented UiBlock

---
+++ Congui/UiBlock.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Congui {
+    public class UiModuleSettings {
+        public char horizontalBorderCharacter = ' ';
+        public char verticalBorderCharacter = ' ';
+        public ConsoleColor borderColor = ConsoleColor.White;
+        public int borderThickness = 1;
+        public bool leftBorderActive = true;
+        public bool topBorderActive = true;
+        public bool rightBorderActive = true;
+        public bool bottomBorderActive = true;
+
+        // Height 0 means each block will be evenly distributed
+        public decimal heightPercentage = 0;
+    }
+
+    public class UiModule {
+
+        // Block variables
+        private int moduleWidth;      // Width (in columns) of the UiModule
+        private int moduleHeight;     // Height (in rows) of the UiModule
+        private int scroll;     // Y offset from the top of the content
+        private int pinLines;   // Pin lines indicates how many lines should be pined (ignored by scroll)
+
+        private List<Tuple<string, ConsoleColor>> content;  // Every individual line inserted in the module
+        private UiModuleSettings settings;                  // Contains all setting for the current module
+
+        public UiModule(int width, int height, UiModuleSettings settings) {
+            this.moduleWidth = width;
+            this.moduleHeight = height;
+            this.settings = settings;
+            this.pinLines = 0;
+
+            // Fail check border properties
+            if (settings.borderThickness * 2 > moduleWidth || settings.borderThickness * 2 > moduleHeight)
+                throw new Exception("Border exceeding module size");
+
+            // Initialize content
+            content = new List<Tuple<string, ConsoleColor>>();
+        }
+
+        public void Flush() {
+            var parsed = CompileSegments();
+            var horizontalBorder = new string(settings.horizontalBorderCharacter, moduleWidth);
+
+            // Visible is a list of all 'visible lines of the module' (lines outside scroll are excluded)
+            var visible = new List<Tuple<string, ConsoleColor, int>>();
+            if (settings.topBorderActive) {
+                for (var i = 0; i < settings.borderThickness; i++)
+                    visible.Add(Tuple.Create(horizontalBorder, settings.borderColor, 0));
+            }
+
+            // Regulate scroll (prevent scroll from going outside)
+            if (scroll >= parsed.Count - GetHeight())
+                scroll = parsed.Count - GetHeight();
+            else if (scroll < 0)
+                scroll = 0;
+
+            if (pinLines >= GetHeight())
+                throw new Exception("Cannot pin more than what's visible");
+
+            var rowsRemaining = GetHeight();
+
+            // Add pinned lines
+            for (var i = 0; i < pinLines; i++) {
+                visible.Add(parsed[i]);
+                rowsRemaining--;
+            }
+        }
+
+        private List<Tuple<string, ConsoleColor, int>> CompileSegments() {
+            var parsed = new List<Tuple<string, ConsoleColor, int>>();
+
+            var innerWidth = GetWidth();
+            var innerHeight = GetHeight();
+
+            // Generate border (vertical & horizontal)
+            var verticalBorder = new string(settings.verticalBorderCharacter, settings.borderThickness);
+            var leftBorder = settings.leftBorderActive ? verticalBorder + " " : "";
+            var rightBorder = settings.leftBorderActive ? verticalBorder + " " : "";
+
+            // Iterate over all text segments and fit them into
+            // the UiModule's dimensions
+            foreach (var segment in content) {
+                var (text, color) = segment;
+
+                // Text segment fits on one line
+                if (text.Length < innerWidth)
+                    parsed = MergeLine(text, innerWidth, parsed, leftBorder, rightBorder, color, segment);
+                else {
+                    var leftOver = text;
+                    while (leftOver.Length > 0) {
+                        // Split segment over multiple lines
+                        if (text.Length - innerWidth >= 0) {
+                            leftOver = text.Substring(innerWidth, text.Length - innerWidth);
+                            text = text.Substring(0, innerWidth);
+                            parsed.Add(Tuple.Create($"{leftBorder}{text}{rightBorder}", color, 0));
+                        }
+                        else {
+                            leftOver = string.Empty;
+                            parsed = MergeLine(text, innerWidth, parsed, leftBorder, rightBorder, color, segment);
+                        }
+                    }
+                }
+            }
+
+            // Complement segments with empty lines if module is not filled
+            if (parsed.Count < innerHeight) {
+                var clearer = new string(' ', innerWidth);
+                while(parsed.Count < innerHeight)
+                    parsed.Add(Tuple.Create($"{leftBorder}{clearer}{rightBorder}", ConsoleColor.White, 0));
+            }
+
+            return parsed;
+        }
+
+        // Merges all borders together with content and a clearer if necessary
+        private static List<Tuple<string, ConsoleColor, int>> MergeLine(
+            string text, int innerWidth, List<Tuple<string, ConsoleColor, int>> parsed, string leftBorder,
+            string rightBorder, ConsoleColor color, Tuple<string, ConsoleColor> segment) {
+
+            // Fill remaining length of line with empty spaces
+            var clearer = new string(' ', innerWidth - text.Length);
+            parsed.Add(new Tuple<string, ConsoleColor, int>($"{leftBorder}{text}{clearer}{rightBorder}", color, 0)); // TODO remember my position? this line might be extended
+            return parsed;
+        }
+
+        // Append text to the content stack of the current module
+        public void Add(string message, ConsoleColor color, bool isCentered = true) {
+            if (isCentered) {
+                var textWidth = moduleWidth;
+                if (settings.leftBorderActive && settings.rightBorderActive)
+                    textWidth = moduleWidth - settings.borderThickness * 2 - 2;
+                else if (settings.leftBorderActive || settings.rightBorderActive)
+                    textWidth = moduleWidth - settings.borderThickness - 1;
+
+                message = UtilityMethods.CenterText(message, textWidth);
+            }
+            content.Add(Tuple.Create(message, color));
+        }
+
+        // Add text to the specified line in the content list (leave index at -1 to target the last line)
+        public void AppendAt(string message, int index = -1) {
+            var (oldText, color) = content[index == -1 ? content.Count - 1 : index];
+            content[index == -1 ? content.Count - 1 : index] = Tuple.Create(oldText + message, color);
+        }
+
+        // Unlike AppendAt modify replaces the text at the targeted position (leave index at -1 to target the last line)
+        public void ModifyAt(string message, ConsoleColor color, int index = -1) {
+            content[index == -1 ? content.Count - 1 : index] = Tuple.Create(message, color);
+        }
+
+        // Remove characters from a specified element in the content list (leave index at -1 to target the last line)
+        public void RemoveAt(int length, int index = -1) {
+            var (oldText, color) = content[index == -1 ? content.Count - 1 : index];
+            if (length > oldText.Length || length < 0) throw new Exception("Out of range");
+            content[index == -1 ? content.Count - 1 : index] = Tuple.Create(oldText.Substring(0, oldText.Length - length), color);
+        }
+
+        public void UpdateDimensions(int width, int height) {
+            this.moduleWidth = width;
+            this.moduleHeight = height;
+        }
+
+        // Clear all content of the module
+        public void Clear() { content.Clear(); }
+        
+        // Reset scroll by setting scroll to 0
+        public void Scroll(int scrollUnits, int pinCount = 0) {
+            if (scrollUnits == 0)
+                scroll = 0;
+
+            scroll += scrollUnits;
+            this.pinLines = pinCount;
+        }
+
+        // ----- [ Getters ] -----
+
+        public int GetWidth() {
+            if (settings.leftBorderActive && settings.rightBorderActive)
+                return moduleWidth - settings.borderThickness * 2 - 2;
+            if (settings.leftBorderActive || settings.rightBorderActive)
+                return moduleWidth - settings.borderThickness - 1;
+            return moduleWidth;
+        }
+
+        public int GetScroll() { return scroll; }
+        public int GetHeight() { return moduleHeight; }
+
+        // --- [ End Getters ] ---
+    }
+}
---
+++ Congui/UtilityMethods.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Congui {
+    internal class UtilityMethods {
+        public static string CenterText(string text, int lineWidth) {
+            if (text.Length > lineWidth)
+                throw new Exception("Text is exceeding line length");
+
+            var extra = lineWidth - text.Length;
+            var padding = "";
+            for (var i = 0; i < extra / 2; i++)
+                padding += ' ';
+            return padding + text + padding + (extra % 2 == 0 ? "" : " ");
+        }
+    }
+}
