719e6496fc637c9dafd61fe222f7b6d402499b06
haholm <57066006+haholm@users.noreply.github.com>
2020-05-29 16:08:35 +0200

    Added mouse input functionality, stylecop config and suppressed rule

---
+++ Congui/Input/Mouse.cs
@@ -0,0 +1,113 @@
+// <copyright file="Mouse.cs" company="Hampfh and haholm">
+// Copyright (c) Hampfh and haholm. All rights reserved.
+// </copyright>
+
+namespace Congui.Input {
+    using System;
+    using System.ComponentModel;
+    using System.Threading.Tasks;
+
+    using Conhics;
+
+    /// <summary>
+    /// Contains functionality for mouse input.
+    /// </summary>
+    public static class Mouse {
+        private static bool isEnabled;
+        private static Task inputListener;
+
+        /// <summary>
+        /// Gets a value indicating whether mouse input can be enabled.
+        /// </summary>
+        /// <value>A value indicating whether mouse input can be enabled.</value>
+        public static bool CanBeEnabled {
+            get {
+                // Returns whether IsCompleted is either true - implying completed, or null - implying inputListener is not initialised.
+                return inputListener?.IsCompleted != false;
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether mouse input is enabled.
+        /// </summary>
+        /// <value>A value indicating whether mouse input is enabled.</value>
+        public static bool IsEnabled {
+            get {
+                return isEnabled;
+            }
+
+            set {
+                if (value && inputListener?.IsCompleted == false) {
+                    throw new Exception($"Mouse input can only be enabled if property CanBeEnabled is true. Property CanBeEnabled is {CanBeEnabled}.");
+                }
+                else if (value && value != isEnabled) {
+                    EnableMouseInput();
+                }
+
+                isEnabled = value;
+            }
+        }
+
+        /// <summary>
+        /// Gets an instance of the <see cref="MouseInput"/> struct with the most recent mouse input.
+        /// </summary>
+        /// <value>An instance of the <see cref="MouseInput"/> struct with the most recent mouse input.</value>
+        public static MouseInput Input { get; private set; }
+
+        private static void EnableMouseInput() {
+            IntPtr inputHandle = Integration.GetStdHandle((int)Integration.StdHandle.InputHandle);
+            ConfigureConsoleMode(inputHandle);
+            inputListener = Task.Run(() => ListenForInput(inputHandle));
+        }
+
+        private static void ConfigureConsoleMode(IntPtr inputHandle) {
+            int consoleMode = 0;
+            ManageNativeReturnValue(
+                returnValue: !Integration.GetConsoleMode(
+                    hConsoleHandle: inputHandle,
+                    lpMode: ref consoleMode));
+            consoleMode |= Integration.ENABLE_MOUSE_INPUT;      // Enable mouse input
+            consoleMode &= ~Integration.ENABLE_QUICK_EDIT_MODE; // Disable quick edit mode (ability to highlight text)
+            consoleMode |= Integration.ENABLE_EXTENDED_FLAGS;   // Enable extended flags for quick edit to take effect
+            ManageNativeReturnValue(
+                returnValue: !Integration.SetConsoleMode(
+                    hConsoleHandle: inputHandle,
+                    dwMode: consoleMode));
+        }
+
+        private static void ListenForInput(IntPtr inputHandle) {
+            Integration.INPUT_RECORD inputRecord = default;
+            uint numberOfInputEvents = 0;
+            uint numberOfInputEventsRead = 0;
+            while (isEnabled) {
+                if (numberOfInputEvents == 0) {
+                    ManageNativeReturnValue(
+                        returnValue: !Integration.GetNumberOfConsoleInputEvents(
+                            hConsoleInput: inputHandle,
+                            lpcNumberOfEvents: out numberOfInputEvents));
+                    if (!isEnabled) {
+                        break;
+                    }
+                    else if (numberOfInputEvents == 0) {
+                        continue;
+                    }
+                }
+
+                ManageNativeReturnValue(
+                    returnValue: !Integration.ReadConsoleInput(
+                        hConsoleInput: inputHandle,
+                        lpBuffer: ref inputRecord,
+                        nLength: 1,
+                        lpNumberOfEventsRead: ref numberOfInputEventsRead /* always equals 1 because nLength: 1 */));
+                numberOfInputEvents -= numberOfInputEventsRead;
+                Input = new MouseInput(inputRecord.MouseEvent);
+            }
+        }
+
+        private static void ManageNativeReturnValue(bool returnValue) {
+            if (!returnValue) {
+                throw new Win32Exception();
+            }
+        }
+    }
+}
---
+++ Congui/Input/MouseInput.cs
@@ -0,0 +1,62 @@
+// <copyright file="MouseInput.cs" company="Hampfh and haholm">
+// Copyright (c) Hampfh and haholm. All rights reserved.
+// </copyright>
+
+namespace Congui.Input {
+    using Conhics;
+
+    /// <summary>
+    /// Contains information regarding mouse input.
+    /// </summary>
+    public struct MouseInput {
+        /// <summary>
+        /// The X position of the mouse from the left.
+        /// </summary>
+        public readonly short X;
+
+        /// <summary>
+        /// The Y position of the mouse from the top.
+        /// </summary>
+        public readonly short Y;
+
+        /// <summary>
+        /// The mouse button that was pressed.
+        /// </summary>
+        public readonly MouseButtons MouseButton;
+
+        /// <summary>
+        /// The mouse event that occured.
+        /// </summary>
+        public readonly MouseEvents MouseEvent;
+
+        /// <summary>
+        /// The mouse scroll wheel direction.
+        /// </summary>
+        public readonly MouseWheelDirections MouseWheelDirection;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MouseInput"/> struct.
+        /// </summary>
+        /// <param name="mouseEventRecord">A MOUSE_EVENT_RECORD object defining mouse event information.</param>
+        internal MouseInput(Integration.MOUSE_EVENT_RECORD mouseEventRecord) {
+            this.X = mouseEventRecord.dwMousePosition.X;
+            this.Y = mouseEventRecord.dwMousePosition.Y;
+            this.MouseButton = (MouseButtons)(mouseEventRecord.dwButtonState & 0x00ff);
+            this.MouseEvent = (MouseEvents)mouseEventRecord.dwEventFlags;
+            this.MouseWheelDirection = MouseWheelDirections.None;
+            int mouseWheelVelocity = mouseEventRecord.dwButtonState >> 16; // Extract the high word value, example: 0x00010004 >> 16 == 0x00000001
+            bool positiveMouseWheelVelocity = mouseWheelVelocity > 0;
+            if (mouseWheelVelocity == 0) {
+                return;
+            }
+            else if (this.MouseEvent == MouseEvents.MouseWheeled) {
+                this.MouseWheelDirection = positiveMouseWheelVelocity ? MouseWheelDirections.Forward : MouseWheelDirections.Backward;
+            }
+            else if (this.MouseEvent == MouseEvents.MouseWheeledHorizontally) {
+                this.MouseWheelDirection = positiveMouseWheelVelocity ? MouseWheelDirections.Right : MouseWheelDirections.Left;
+            }
+        }
+
+        // TODO: public MouseInput(...) { ... }
+    }
+}
